d64c887b84b741356059ece24f1ad73b40c12199
Ian Kiema <112932127+iankiema@users.noreply.github.com>
2023-06-26 15:48:44 +0300

    Updated the app.js file

--- app.js
+++ app.js
@@ -4,10 +4,10 @@ const mobileMenuCont = document.querySelector('.mobile-menu-contaner');
 const mainbar = document.querySelector('.main-bar');
 const linksa = document.querySelector('.mobile-menu-contaner ul ');
 
-menuIcon.addEventListener('click', () => {
-  mobileMenuCont.classList.add('active');
-  mainbar.classList.add('active');
-});
+// menuIcon.addEventListener('click', () => {
+//   mobileMenuCont.classList.add('active');
+//   mainbar.classList.add('active');
+// });
 closeIcon.addEventListener('click', () => {
   mobileMenuCont.classList.remove('active');
   mainbar.classList.remove('active');
