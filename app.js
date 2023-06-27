715fbbdd185bf8833e6b79afdaf11fa56ad90ffb
redwan <redwanthr@gmail.com>
2023-06-27 08:20:18 -0700

    Add index.html, Style.css, app.js and all images

--- app.js
+++ app.js
@@ -17,3 +17,129 @@ linksa.addEventListener('click', () => {
   mobileMenuCont.classList.remove('active');
   mainbar.classList.remove('active');
 });
+
+
+
+function toggle(num){
+  const popup2 = document.getElementById('popup2');
+  popup2.classList.toggle('active');
+  renderProdcuts(num)
+}
+
+const cards = [
+  {
+    id: 0,
+    name: "Tonic ",
+    techolgies: ["HTML","CSS","JAVAScript","Ruby","BootStrap"],
+    Dtecholgies:["CANOPY", "Back End Dev","2015"],
+    description:
+     [ " Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has  been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s. Lorem Ipsum is simply dummy text of the printing and  typesetting industry. "
+    ," Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releorem Ipsum is simply dummy text  of the printing and typesetting industry. Lorem Ipsum han printer took a galley of type and scrambled it 1960 "],
+     imgSrc: "./image/SnapshootPortfolio.png",
+    linkstolv:"See Lives ",
+    linkstos:"See Sources",
+  },
+  {
+    id: 1,
+    name: "Multi-Post Stories",
+    techolgies: ["HTML","CSS","JAVAScript","Ruby","BootStrap"],
+    Dtecholgies:["FACEBOOK", "Back End Dev","2015"],
+    description:
+    [ " Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has  been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s. Lorem Ipsum is simply dummy text of the printing and  typesetting industry. "
+   ," Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releorem Ipsum is simply dummy text  of the printing and typesetting industry. Lorem Ipsum han printer took a galley of type and scrambled it 1960 "],
+    imgSrc: "./image/SnapshootPortfoliop2.png",
+   linkstolv:"See Lives ",
+   linkstos:"See Sources",
+  },
+  {
+    id: 1,
+    name: "Facebook 360",
+    techolgies: ["HTML","CSS","JAVAScript","Ruby","BootStrap"],
+    Dtecholgies:["FACEBOOK", "Back End Dev","2015"],
+    description:
+    [ " Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has  been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s. Lorem Ipsum is simply dummy text of the printing and  typesetting industry. "
+   ," Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releorem Ipsum is simply dummy text  of the printing and typesetting industry. Lorem Ipsum han printer took a galley of type and scrambled it 1960 "],
+    imgSrc: "./image/SnapshootPortfoliop3.png",
+   linkstolv:"See Lives ",
+   linkstos:"See Sources",
+  },
+  {
+    id: 1,
+    name: "Uber Navigation",
+    techolgies: ["HTML","CSS","JAVAScript","Ruby","BootStrap"],
+    Dtecholgies:["Uber", "Lead Dev","2015"],
+    description:
+     [ " Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has  been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s. Lorem Ipsum is simply dummy text of the printing and  typesetting industry. "
+    ," Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releorem Ipsum is simply dummy text  of the printing and typesetting industry. Lorem Ipsum han printer took a galley of type and scrambled it 1960 "],
+     imgSrc: "./image/SnapshootPortfoliop4.png",
+    linkstolv:"See Lives ",
+    linkstos:"See Sources",
+  },
+];
+
+// SELECT ELEMENTS
+const cardsE = document.querySelector(".popup2")
+
+// RENDER PRODUCTS
+function renderProdcuts(num) {
+
+  carde=Object.values(cards)[num];
+  cardsE.innerHTML = `
+  <div class="header">
+  <div class="left">
+      <h3>${carde.name}</h3>
+      <ul>
+          <li>${carde.Dtecholgies[0]}</li>
+          <li>${carde.Dtecholgies[1]}</li>
+          <li>${carde.Dtecholgies[2]}</li>
+      </ul>
+  </div>
+  <div class="right">
+  <a href="#" onclick="toggle()"><img src="./image/Icon2.png" alt=""></a>
+      
+  </div>
+  </div>
+  <div class="middle">
+  
+      <img src="${carde.imgSrc}" alt="">
+  </div>
+
+  <div class="contents">
+      <div class="left">
+          <p>
+              ${carde.description[0]}
+          </p>
+          <div class="top">
+              <p >
+              ${carde.description[1]}
+              </p>
+           </div>
+          
+                  
+      </div>
+      <div class="right">
+          <div class="links">
+              <ul>
+                  <li>${carde.techolgies[0]}</li>
+                  <li>${carde.techolgies[1]}</li>
+                  <li>${carde.techolgies[2]}</li>
+                  <li>${carde.techolgies[3]}</li>
+                  <li>${carde.techolgies[4]}</li>
+              </ul>
+          </div>
+          <div class="lines">
+            <br>
+             <hr>
+          </div>
+          <div>
+              <img src="Vector 4.png" alt="">
+          </div>
+          <div class="buttons">
+          <button class="links-live">${carde.linkstolv}<img src="./image/See-live-Icon.png" alt=""> </button>
+          <button class="links-source">${carde.linkstos}<img src="./image/see-sourec-Vector" alt=""></button>
+          </div>
+      </div>
+      
+  </div>
+          `;
+}
