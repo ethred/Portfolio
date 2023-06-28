896edb0f471ae33035dcce7877af410cc4aabc2b
redwan <redwanthr@gmail.com>
2023-06-28 13:44:50 -0700

    Update style.css, index.html, app.js

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const mobileMenuCont = document.querySelector('.mobile-menu-contaner');
 const mainbar = document.querySelector('.main-bar');
 const linksa = document.querySelector('.mobile-menu-contaner ul ');
 
+// const scrollb=document.querySelector('body');
+// body.style.overflow = "hidden";
+
 menuIcon.addEventListener('click', () => {
   mobileMenuCont.classList.add('active');
   mainbar.classList.add('active');
@@ -22,7 +25,7 @@ const cards = [
   {
     id: 0,
     name: 'Tonic ',
-    techolgies: ['HTML', 'CSS', 'JAVAScript', 'Ruby', 'BootStrap'],
+    techolgies: ['HTML', 'CSS', 'JAVAScript', 'Ruby'],
     Dtecholgies: ['CANOPY', 'Back End Dev', '2015'],
     description:
      [" Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has  been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s. Lorem Ipsum is simply dummy text of the printing and  typesetting industry. ",
@@ -34,7 +37,7 @@ const cards = [
   {
     id: 1,
     name: 'Multi-Post Stories',
-    techolgies: ['HTML', 'CSS', 'JAVAScript', 'Ruby', 'BootStrap'],
+    techolgies: ['HTML', 'Ruby on ralls', 'CSS', 'JAVAScript'],
     Dtecholgies: ['FACEBOOK', 'Back End Dev', '2015'],
     description:
     [" Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has  been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s. Lorem Ipsum is simply dummy text of the printing and  typesetting industry. ",
@@ -46,7 +49,7 @@ const cards = [
   {
     id: 1,
     name: 'Facebook 360',
-    techolgies: ['HTML', 'CSS', 'JAVAScript', 'Ruby', 'BootStrap'],
+    techolgies: ['HTML', 'Ruby on ralls', 'CSS', 'JAVAScript'],
     Dtecholgies: ['FACEBOOK', 'Back End Dev', '2015'],
     description:
     [" Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has  been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s. Lorem Ipsum is simply dummy text of the printing and  typesetting industry. ",
@@ -58,7 +61,7 @@ const cards = [
   {
     id: 1,
     name: 'Uber Navigation',
-    techolgies: ['HTML', 'CSS', 'JAVAScript', 'Ruby', 'BootStrap'],
+    techolgies: ['HTML', 'Ruby on ralls', 'CSS', 'JAVAScript'],
     Dtecholgies: ['Uber', 'Lead Dev', '2015'],
     description:
      [" Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has  been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s. Lorem Ipsum is simply dummy text of the printing and  typesetting industry. ",
@@ -86,7 +89,7 @@ function renderProdcuts(num) {
       </ul>
   </div>
   <div class="right">
-  <a href="#" onclick="toggle()"><img src="./image/Icon2.png" alt=""></a>
+  <a href="#"  onclick="togglee()" ><img src="./image/Icon2.png" alt=""></a>
       
   </div>
   </div>
@@ -115,7 +118,6 @@ function renderProdcuts(num) {
                   <li>${carde.techolgies[1]}</li>
                   <li>${carde.techolgies[2]}</li>
                   <li>${carde.techolgies[3]}</li>
-                  <li>${carde.techolgies[4]}</li>
               </ul>
           </div>
           <div class="lines">
@@ -135,4 +137,71 @@ function renderProdcuts(num) {
           `;
 }
 
+const cardsEI = document.querySelector('.works');
+// RENDER PRODUCTS
+function rendercards() {
+  let con = true;
+  let cont = 0;
+  let passvalue = 0;
+  let x = 'one';
+  let imgid = 'img1';
+
+  cards.forEach((product) => {
+    if (con === true) {
+      x = 'one';
+      con = false;
+    } else {
+      x = 'two';
+      con = true;
+    }
+    if (cont === 0) {
+      imgid = 'img1';
+      cont += 1;
+    } else if (cont === 1) {
+      imgid = 'img2';
+      cont += 1;
+    } else if (cont === 2) {
+      imgid = 'img3';
+      cont += 1;
+    } else if (cont === 3) {
+      imgid = 'img4';
+      cont += 1;
+    }
+
+    cardsEI.innerHTML += `
+                  <nav class="card" id="${x}">
+                  <div id="${imgid}"></div>
+                  <div class="left-bloks" id="sub-left-block">
+                    <div class="primary">
+                      <h1>${product.name}</h1>
+                      <div class="frame">
+                        <p id="font">${product.Dtecholgies[0]}</p>
+                        <img src="./image/Counter.png" alt="counter" />
+                        <h6>${product.Dtecholgies[1]}</h6>
+                        <img src="./image/Counter.png" alt="counter" />
+                        <h6>${product.Dtecholgies[2]}</h6>
+                      </div>
+                    </div>
+                    <p class="text">
+                      A daily selection of privately personalized reads; no accounts or
+                      sign-ups required.
+                    </p>
+                    <ul>
+                        <li><a href="#">${product.techolgies[0]}</a></li> 
+                        <li><a href="#">${product.techolgies[1]}</a></li> 
+                        <li><a href="#">${product.techolgies[2]}</a></li> 
+                        <li><a href="#">${product.techolgies[3]}</a></li> 
+
+                    </ul>
+                    <div class="action">
+                      <button type="submit" class="btn" onclick="toggle(${passvalue})""><p>See project</p></button>
+                    </div>
+                  </div>
+                </nav>
+        `;
+    passvalue += 1;
+  });
+}
+
+rendercards();
 renderProdcuts();
