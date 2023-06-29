f1c0c997785a38d5057f8c216d7be2d0a6b17358
redwan <redwanthr@gmail.com>
2023-06-29 07:47:07 -0700

    update index.html, style.css, app.js

--- app.js
+++ app.js
@@ -200,4 +200,46 @@ function rendercards() {
 }
 
 rendercards();
+
+/* Form validation */
+
+const form = document.querySelector('form');
+const email = document.getElementById('email');
+const submitBtn = document.getElementById('contact-btn');
+const error = submitBtn.nextElementSibling;
+const inputs = document.querySelectorAll('input');
+const messageArea = document.getElementById('textarea');
+const emailRegExp = /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$/;
+// const lettercase =/^[a-z]/;
+
+window.addEventListener('load', () => {
+  const isValid = email.value.length === 0 || emailRegExp.test(email.value);
+  email.className = isValid ? 'valid' : 'invalid';
+});
+
+email.addEventListener('input', () => {
+  const isValid = email.value.length === 0 || emailRegExp.test(email.value);
+  if (isValid) {
+    email.className = 'valid';
+    error.textContent = '';
+    error.className = 'error';
+  } else {
+    email.className = 'invalid';
+  }
+});
+
+// form.addEventListener('submit', (event) => {
+//   event.preventDefault();
+
+//   const isValid = email.value.length === 0 || emailRegExp.test(email.value);
+//   if (!isValid) {
+//     email.className = 'invalid';
+//     error.textContent = 'The email text has to be in small letters or Invalid e-mail';
+//     error.className = 'error active';
+//   } else {
+//     email.className = 'valid';
+//     error.textContent = '';
+//     error.className = 'error';
+//   }
+// });
 renderProdcuts();
